5f561c8d6c7e3f0d021815f339ebac8e4efeff0d
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-01-09 11:14:48 -0400

    GoTop feita

---
+++ src/components/GoTop/stories.jsx
@@ -0,0 +1,211 @@
+import { GoTop } from ".";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "GoTop",
+  component: GoTop,
+  args: {
+    children: "GoTop",
+  },
+  argTypes: {
+    children: { type: "string" },
+  },
+};
+export const Template = (args) => {
+  return (
+    <div style={{ height: "400vh" }}>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <h1>Lorem ipsum dolor sit amet consectetur i?</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia qui impedit inventore quos adipisci esse
+        ex illum asperiores nemo sit quaerat placeat laborum repellat non nulla sint, nostrum perspiciatis.
+        Qui?
+      </p>
+      <GoTop {...args} />
+    </div>
+  );
+};

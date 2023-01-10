cd2de8ab7b8a1719fe9662f82e6fa8f4c29115ca
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-01-10 14:52:31 -0400

    a

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import { Heading } from "../../components/Heading";
 import { Base } from "../Base";
 import { mockBase } from "../Base/mock";
 import * as Styled from "./styles";
+import { mapData } from "../../api/map-data";
 
 function Home() {
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch(
+        "http://localhost:1337/api/pages/?filters=[slug]=landing-page&populate[sections][populate]=*",
+      );
+      const json = await data.json();
+      const pagesDataArray = Array.isArray(json) ? json : [json];
+      const pageData = mapData(pagesDataArray);
+
+      console.log(pageData);
+    };
+
+    load();
+  }, []);
+
   return <Base {...mockBase} />;
 }
 
